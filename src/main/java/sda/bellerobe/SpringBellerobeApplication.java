59a364242be2f3a1c1ab3d3ae1291bd7557ae202
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-06-06 16:47:35 -0600

    first commit

---
+++ src/main/java/sda/bellerobe/SpringBellerobeApplication.java
@@ -0,0 +1,13 @@
+package sda.bellerobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBellerobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBellerobeApplication.class, args);
+	}
+
+}
