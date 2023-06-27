17fa9cece4da5517498a78ed4165adf437fa275f
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-06-27 17:01:10 -0600

    vestido

--- src/main/java/sda/bellerobe/model/VestidoModel.java
+++ src/main/java/sda/bellerobe/model/VestidoModel.java
@@ -1,5 +1,39 @@
 package sda.bellerobe.model;
 
-public class VestidoModel {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table (name = "TAB_vestido") 
+public class VestidoModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_vestido")
+	private Integer idVestido;
+	
+	@Column(name = "nombrevestido")
+	private Integer nombreVestido;
+	
+	@Column(name = "descripcion")
+	private Integer descripcion;
+	
+	@Column(name = "valoracion")
+	private Integer valoracion;
+	
+	@Column(name = "precio")
+	private Integer precio;
+	
+	@Column(name = "id_tipo")
+	private Integer id_tipo;
+	
 }
