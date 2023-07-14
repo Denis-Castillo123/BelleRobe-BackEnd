1671d7c07cc43a5f406b3dac33b16c7d0d4da67e
Denis-Castillo123 <90869371+Denis-Castillo123@users.noreply.github.com>
2023-07-14 14:32:45 -0600

    usuarios

--- src/main/java/sda/bellerobe/controller/UsuarioController.java
+++ src/main/java/sda/bellerobe/controller/UsuarioController.java
@@ -5,11 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sda.bellerobe.model.Response;
 import sda.bellerobe.model.UsuarioModel;
+import sda.bellerobe.model.UsuarioRequest;
+import sda.bellerobe.model.VestidoModel;
+import sda.bellerobe.model.VestidoRequest;
 import sda.bellerobe.service.IUsuarioService;
 
 @CrossOrigin(origins = "*")
@@ -20,7 +28,7 @@ public class UsuarioController {
 	@Autowired
 	private IUsuarioService usuarioService;
 	
-	@GetMapping(path= "/consultar", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path= "/consultarTodos", produces = MediaType.APPLICATION_JSON_VALUE)
 	private ResponseEntity<Response<UsuarioModel>> consultarTodos(){
 		
 		Response<UsuarioModel> response = usuarioService.consultarTodos();
@@ -29,6 +37,33 @@ public class UsuarioController {
 		
 	}
 	
+	@PostMapping(path = "/guardarUsuario", produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response <UsuarioModel>> guardarUsuario (@RequestBody UsuarioRequest usuario){
+		
+		Response<UsuarioModel> response = usuarioService.guardarUsuario(usuario);
+		
+		return new ResponseEntity<Response<UsuarioModel>>(response,HttpStatus.OK);
+		
+	}
 	
+	@DeleteMapping(path = "/borrarUsuarioPorId/{idUsuario}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response<Integer>> borrarVestidoPorId (@PathVariable("idUsuario") Integer idUsuario){
+		
+		Response<Integer> response = usuarioService.eliminarUsuario(idUsuario);
+		
+		return new ResponseEntity<Response <Integer>>(response, HttpStatus.OK);
+	}
+	
+	@PutMapping(path = "/actualizarVestido",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response <UsuarioModel>> actualizarVestido (@RequestBody UsuarioRequest usuario){
+	
+		Response<UsuarioModel> response = usuarioService.actualizarUsuario(usuario);
+		
+		return new ResponseEntity<Response<UsuarioModel>> (response, HttpStatus.OK);
+	}
 
 }
