7b1d1a4ae24463f467d756b509224b59e8375445
LuisGusDev <luisggc321@gmail.com>
2023-07-31 14:27:03 -0600

    Reestructuracion para integración de funcionalidades de administrador, autenticación, imagenes, JWT

--- src/main/java/sda/bellerobe/repository/CiudadRepository.java
+++
@@ -1,9 +0,0 @@
-package sda.bellerobe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sda.bellerobe.model.CatCiudadModel;
-
-public interface CiudadRepository extends JpaRepository<CatCiudadModel,Integer>{
-
-}
--- src/main/java/sda/bellerobe/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package sda.bellerobe.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sda.bellerobe.model.UsuarioModel;
-
-public interface UsuarioRepository extends JpaRepository<UsuarioModel, Integer>{
-
-}
